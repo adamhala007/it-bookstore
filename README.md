d1f840c8efcd51ee23715951d6d98c16ef3b9aa1
adamhala007 <adamhala007@gmail.com>
2023-03-10 21:01:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# it_bookstore
+
+Application for IT books
---
+++ android/app/src/main/java/com/example/it_bookstore/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.it_bookstore;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
